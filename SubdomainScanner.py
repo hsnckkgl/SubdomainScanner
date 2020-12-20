dc5197c8f5637c9efd0bf4aef47a09c413e7c26b
hsnckkgl <hsnckkgl@gmail.com>
2020-12-20 15:10:10 +0000

    done

---
+++ SubdomainScanner.py
@@ -0,0 +1,17 @@
+import requests
+import sys
+
+#subdomains-10000.txt from https://github.com/rbsec/dnscan/blob/master/subdomains-10000.txt
+sublist = open("subdomains-10000.txt").read()
+subs = sublist.splitlines()
+
+for sub in subs:
+    urltocheck = f"http://{sub}.{sys.argv[1]}"
+
+    try:
+        requests.get(urltocheck)
+
+    except requests.ConnectionError:
+        pass
+    else:
+        print("Valid domain: ", urltocheck)
